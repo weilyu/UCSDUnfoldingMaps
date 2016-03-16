b6cdafbdeff0e28109d628ca9a6340d601bceb48
Wei Lyu <lvwei1001@live.com>
2016-03-16 17:40:28 +0800

    make cityMarker draw triangle

--- src/module4/CityMarker.java
+++ src/module4/CityMarker.java
@@ -43,6 +43,8 @@ public class CityMarker extends SimplePointMarker {
 		pg.pushStyle();
 		
 		// TODO: Add code to draw a triangle to represent the CityMarker
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y-10, (float)(x + 5 * 1.73), (float)(y + 5), (float)(x - 5 * 1.73), (float)(y + 5));
 		
 		// Restore previous drawing style
 		pg.popStyle();
